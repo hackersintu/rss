74eccbb1474c7ebc611d63251089f6ca5d340b48
Sintu singh <58273031+hackersintu@users.noreply.github.com>
2021-02-27 07:29:57 +0530

    Initialize 0.5-stable

---
+++ grunt/config/phantom.js
@@ -0,0 +1,11 @@
+var grunt = require("grunt");
+
+exports.run = {
+  port: 8080,
+  harness: "test/phantom-harness.js",
+  // Run `grunt test --debug` to enable in-browser testing.
+  debug: !!grunt.option("debug"),
+  tests: [
+    "**/__tests__/*-test.js"
+  ]
+};
