100d4c192bd3518215eb52dbb4ed65fee4ac8ebb
Sintu singh <58273031+hackersintu@users.noreply.github.com>
2021-02-27 07:35:22 +0530

    Initialize gaearon-patch-5

---
+++ packages/legacy-events/ReactSyntheticEventType.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Flow type for SyntheticEvent class that includes private properties
+ * @flow
+ */
+
+import type {Fiber} from 'react-reconciler/src/ReactFiber';
+import type {EventPriority} from 'shared/ReactTypes';
+import type {TopLevelType} from './TopLevelEventTypes';
+
+export type DispatchConfig = {|
+  dependencies?: Array<TopLevelType>,
+  phasedRegistrationNames: {|
+    bubbled: null | string,
+    captured: null | string,
+  |},
+  registrationName?: string,
+  eventPriority: EventPriority,
+|};
+
+export type CustomDispatchConfig = {|
+  phasedRegistrationNames: {|
+    bubbled: null,
+    captured: null,
+  |},
+  customEvent: true,
+|};
+
+export type ReactSyntheticEvent = {|
+  dispatchConfig: DispatchConfig | CustomDispatchConfig,
+  getPooled: (
+    dispatchConfig: DispatchConfig | CustomDispatchConfig,
+    targetInst: Fiber,
+    nativeTarget: Event,
+    nativeEventTarget: EventTarget,
+  ) => ReactSyntheticEvent,
+  isPersistent: () => boolean,
+  _dispatchInstances: null | Array<Fiber | EventTarget> | Fiber | EventTarget,
+  _dispatchListeners: null | Array<Function> | Function,
+  _targetInst: Fiber,
+  type: string,
+|};
