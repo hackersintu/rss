1df56ced561b715c60958734ea251875f40c2b13
Sintu singh <58273031+hackersintu@users.noreply.github.com>
2021-02-27 07:41:23 +0530

    Initialize Rs

---
+++ packages/scheduler/src/SchedulerFeatureFlags.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+export const enableSchedulerDebugging = false;
+export const enableIsInputPending = false;
+export const enableProfiling = __VARIANT__;
+
+// TODO: enable to fix https://github.com/facebook/react/issues/20756.
+export const enableSetImmediate = __VARIANT__;
